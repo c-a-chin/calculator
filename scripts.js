4855342aa677d2207081d16f5d138a37f902d80c
Cameron Chin <cameron.a.chin@gmail.com>
2018-05-21 04:53:30 -0400

    fixed display bugs, added styling

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 // all numbers, operators and the decimal point are "main-buttons", all other buttons have ids
 // text of operator buttons have spaces on ends, i.e " + " instead of "+" for design
+// max length for calculator display is 16em
 
 // store current operation/previous operation as strings
 var currentOperation  = "",
@@ -11,6 +12,10 @@ var answer = "";
 // on main button click, add text of button to main display
 $(".main-buttons").on("click", function(){
 	var text = $(this).text();
+	// if max length is exceeded, dont add any more characters
+	if(currentOperation.length+1 > 16){
+		return; //do nothing
+	}
 	// if input is operator, and last input was operator (2 in a row), dont add to operation
 	if((currentOperation[currentOperation.length-2] == "x" || 
 	    currentOperation[currentOperation.length-2] == "÷" || 
@@ -50,12 +55,21 @@ $("#equal").on("click", function(){
 		if(currentOperation[i] == "÷") currentOperation[i] = "/";
 	}
 	currentOperation = currentOperation.join("");
-	// evaluate and display answer
+
+	// evaluate and round answer
 	answer = eval(currentOperation);
-	previousOperation += answer;
-	$("#main-display").text(answer);
-	$("#sub-display").text(previousOperation);
-	currentOperation = "";
+	// round to two decimal places
+	answer = Math.round(answer * 100) / 100;
+	// if answer is NaN, reset, else display the answer
+	if(!answer){
+		$("#ac").click();
+	} else {
+		previousOperation += answer;
+		$("#main-display").text(answer);
+		$("#sub-display").text(previousOperation);
+		currentOperation = "";
+	}
+	
 })
 
 
@@ -64,8 +78,10 @@ $("#flip-screen").on("click", function(){
 	$("#screen").toggleClass("flip-screen");
 })
 
-// words that can be typed on calculator
-const words = ["0.1134", "379009", "58008", "5318008", "37047734", "618", "32008", "663", "5663", "376616", "38076", "553580", "3045", "53045"]
+// words that can be typed on calculator (for random word button)
+const words = ["01134", "379009", "58008", "618", "32008", "5318804",
+				"663", "5663", "376616", "38076", "553580", "3045", "53045", 
+				"338", "55178", "376608", "063", "35336", "378163771", "3781637"]
 // random number function
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
